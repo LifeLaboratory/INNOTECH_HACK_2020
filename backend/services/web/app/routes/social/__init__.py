6189af6eabff62a71df4d979ffddc73734726541
Boris Bochkaryov <Boris-Bochkaryov@yandex.ru>
2020-11-28 21:58:14 +0300

    Возврат интересов пользователя

--- backend/services/web/app/routes/social/__init__.py
+++ backend/services/web/app/routes/social/__init__.py
@@ -1,11 +1,9 @@
 from flask_restplus import Resource, reqparse
 
 from app.config import VK_LOGIN, VK_PASSWORD
-from app.routes.egrul_nalog.provider import Provider
-from app.routes.egrul_nalog.models import *
 from app import ns_api
-from app.utils.clients.egrul_nalog import EgrulNalogClient
 import vk_api
+import re
 
 vk_session = vk_api.VkApi(VK_LOGIN, VK_PASSWORD)
 vk_session.auth()
@@ -16,7 +14,20 @@ vk = vk_session.get_api()
 @ns_api.route('/social/user/<int:user_id>', methods=['GET'])
 class Social(Resource):
     def get(self, user_id):
+        groups = vk.groups.get(user_id=str(user_id), extended=1)
+        words = {}
+        for obj in groups['items']:
+            for word in obj['name'].split(' '):
+                if word == '|' or len(word) < 4 or re.match(r'(^[а-яА-Я]+(ый|ые)$|[0-9]+)', word) is not None or word == 'сообщество' or word == 'Сообщество':
+                    continue
+                if words.get(word) is None:
+                    words[word] = 0
+                words[word] = words.get(word) + 1
+
+        interests = [X for X, Y in sorted(words.items(), key=lambda x: x[1], reverse=True)]
+
         return {
             'ID': user_id,
-            'USER': vk.users.get(user_ids=str(user_id), fields='photo_max,sex,bdate,contacts,site,photo_max_orig')
+            'USER': vk.users.get(user_ids=str(user_id), fields='photo_max,sex,bdate,contacts,site,photo_max_orig'),
+            'INTERESTS': interests[:10]
         }
