4899493277a7606044c83ce1ba70fe307a68dbd7
Anton Zeunov <lalow.main@gmail.com>
2020-11-28 21:05:05 +0700

    init

---
+++ frontd/src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '@/components/navs/Home.vue'
+import Homes from '@/views/Home.vue'
+import URLPage from '@/views/URLPage.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    children: [
+      {
+        path: 'photo',
+        name: 'Home',
+        component: Homes,
+      },
+      {
+        path: 'url',
+        name: 'URL',
+        component: URLPage,
+      }
+    ]
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/About.vue')
+    }
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ frontd/src/views/URLPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    Введите ссылку на VK человека
+     <a-input placeholder=""  style="margin-left: 50px; margin-rigth: 50px; margin-top: 20px; margin-bottom: 20px"/>
+     <a-button type="primary">
+      Поиск
+    </a-button>
+  </div>
+</template>
