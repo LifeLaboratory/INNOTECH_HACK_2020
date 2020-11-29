a42fea030a81ee36b697d50008d351ddeb931e1b
NzKoff <nskvip54@gmail.com>
2020-11-29 10:20:58 +0700

    Добавил функцию - add_organization_by_inn_fl(inn) - добавляет Организации Физ. Лица по ИНН

--- backend/services/web/app/base/sql_queries.py
+++ backend/services/web/app/base/sql_queries.py
@@ -1,17 +1,17 @@
 SELECT_ORGANIZATIONS_BY_CLIENT = """
 SELECT * FROM public.organization o
-LEFT JOIN public.link_client_organization l ON (o.id = l.organization_id)
+LEFT JOIN public.link_client_organization l ON (o.organization_id = l.organization_id)
 WHERE l.client_id = {client_id};
 """
 
 SELECT_ORGANIZATION_BY_INN = """
-SELECT id FROM public.organization
-WHERE inn_org = {inn_org};
+SELECT organization_id FROM public.organization
+WHERE organization_inn = {organization_inn};
 """
 
 SELECT_CLIENT = """
 SELECT * FROM public.client
-WHERE id={id};
+WHERE client_id={client_id};
 """
 
 SELECT_TERRORIST = """
@@ -25,19 +25,19 @@ FROM public.client;
 """
 
 INSERT_CLIENT = """
-INSERT INTO public.client (surname, name, patronymic, nationality, country, inn, inn_reg)
-VALUES ('{surname}', '{name}', '{patronymic}', '{nationality}', '{country}', {inn}, '{inn_reg}')
-RETURNING id;
+INSERT INTO public.client (surname, name, patronymic, nationality, country, inn, inn_reg, sex, city, bdate, phone)
+VALUES ('{surname}', '{name}', '{patronymic}', '{nationality}', '{country}', {inn}, '{inn_reg}', {sex}, '{city}', '{bdate}', {phone})
+RETURNING client_id;
 """
 
 INSERT_ORGANIZATION = """
-INSERT INTO public.organization (name_org, url_org, status_org, leader_org, date_reg_org, inn_org)
-VALUES ('{name_org}', '{url_org}', '{status_org}', '{leader_org}', '{date_reg_org}', {inn_org})
-RETURNING id;
+INSERT INTO public.organization (organization_name, organization_url, organization_status, organization_leader, organization_date_reg, organization_inn)
+VALUES ('{organization_name}', '{organization_url}', '{organization_status}', '{organization_leader}', {organization_date_reg}, {organization_inn})
+RETURNING organization_id;
 """
 
 INSERT_LINK_CLIENT_ORGANIZATION = """
-INSERT INTO public.link_client_organization (client_id, orgabization_id, status_client)
+INSERT INTO public.link_client_organization (client_id, organization_id, status_client)
 VALUES ({client_id}, {organization_id}, '{status_client}')
-RETURNING id;
+RETURNING client_id;
 """
--- backend/services/web/app/utils/sql_functioin.py
+++ backend/services/web/app/utils/sql_functioin.py
@@ -1,17 +1,38 @@
 from app.base import sql_queries, base_sql
+from app.utils.parsers import parse_egrul_egrip as pee
 
 
 def add_user(params):
     answer = base_sql.Sql.exec(sql_queries.INSERT_CLIENT.format(**params))
-    return answer[0]["id"]
+    return answer[0]["client_id"]
 
 
 def add_organization(params):
     answer = base_sql.Sql.exec(sql_queries.SELECT_ORGANIZATION_BY_INN.format(**params))
-    if answer is None or answer == "":
+    if answer is None or answer == []:
+        params["organization_date_reg"] = "NULL"
         answer = base_sql.Sql.exec(sql_queries.INSERT_ORGANIZATION.format(**params))
-    if answer is not None and answer != "":
-        params.update({'organization_id': answer[0]['id']})
+    if answer is not None and answer != []:
+        params.update({'organization_id': answer[0]['organization_id']})
         answer = base_sql.Sql.exec(sql_queries.INSERT_LINK_CLIENT_ORGANIZATION.format(**params))
     return answer
 
+
+def add_organiztion_by_inn_fl(inn):
+    answer = pee.parse_egrul_egrip(inn)
+    if "list_ip" in answer.keys():
+        for i in answer["list_ip"]:
+            i["status_client"] = "list_ip"
+            i["client_id"] = 1
+            add_organization(i)
+    if "list_leader" in answer.keys():
+        for i in answer["list_leader"]:
+            i["client_id"] = 1
+            i["status_client"] = "list_leader"
+            add_organization(i)
+    if "list_founder" in answer.keys():
+        for i in answer["list_founder"]:
+            i["client_id"] = 1
+            i["status_client"] = "list_founder"
+            add_organization(i)
+    return True
